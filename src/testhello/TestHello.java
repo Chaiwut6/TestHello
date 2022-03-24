1f1b685a7f97b7e6f00c9c285a50c9def257d148
Chaiwut6 <chivwut0646@gmail.com>
2022-03-24 10:33:59 +0700

    first commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -7,6 +7,8 @@ public class TestHello {
     
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Welcome to Thailand");
+        
     }
     
 }
