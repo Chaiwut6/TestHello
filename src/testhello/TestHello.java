9befef2e6a6b7fadd6536b5319598ff409fa4fb1
Chaiwut6 <chivwut0646@gmail.com>
2022-03-24 10:37:10 +0700

    frist commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -8,6 +8,9 @@ public class TestHello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Welcome to Thailand");
+        System.out.println("Have a good trip");
+        System.out.println("wite a minute");
+        System.out.println("Hi people");
         
     }
     
